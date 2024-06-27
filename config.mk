67c02d45089af7f8cb6c7014fba794b8ea7bfcab
Marco Julian Solanki <msolanki@student.ethz.ch>
2024-06-27 23:50:50 +0200

    Move OpenACC Make variables to config.mk

--- config.mk
+++ config.mk
@@ -3,6 +3,9 @@ REALSIZE=8
 CXX=clang++
 CXXFLAGS=-std=c++11 -Ofast -march=native -Wall -Wextra -Wpedantic -Wshadow
 
+ACCCXX=CC
+ACCFLAGS=$(CXXFLAGS) -fopenacc
+
 CUDACXX=nvcc
 CUDAFLAGS=-std=c++11 -O3 --use_fast_math -dlto -arch=sm_60 -Xcompiler "-Ofast -march=native -Wall -Wextra -Wshadow"
 
