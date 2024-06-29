c0408d80a22b2cc2cc21e683bc2b98846cc07778
Marco Julian Solanki <msolanki@student.ethz.ch>
2024-06-29 22:30:05 +0200

    Get OpenACC C++ samples to compile on Piz Daint

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ CXX=clang++
 CXXFLAGS=-std=c++11 -Ofast -march=native -Wall -Wextra -Wpedantic -Wshadow
 
 ACCCXX=nvc++
-ACCFLAGS=$(CXXFLAGS) -acc -gpu=managed -Minfo=accel
+ACCFLAGS=-std=c++11 -tp=native -acc=gpu -gpu=managed # -O4 -gpu=cc60 -Minfo=accel
 
 CUDACXX=nvcc
 CUDAFLAGS=-std=c++11 -O3 --use_fast_math -dlto -arch=sm_60 -Xcompiler "-Ofast -march=native -Wall -Wextra -Wshadow"
