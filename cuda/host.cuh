85105fbb71371f43192291a77bdf5ad6aad2c8a1
Marco Julian Solanki <msolanki@student.ethz.ch>
2024-07-14 17:00:05 +0200

    Remove faulty casts from C++ initialisation codes

--- cuda/host.cuh
+++ cuda/host.cuh
@@ -18,12 +18,12 @@ namespace host {
 template<typename T>
 void initialise(T *u_host, std::size_t xsize, std::size_t ysize, std::size_t zsize) {
 
-    const std::size_t imin = static_cast<T>(0.25 * xsize + 0.5);
-    const std::size_t imax = static_cast<T>(0.75 * xsize + 0.5);
-    const std::size_t jmin = static_cast<T>(0.25 * ysize + 0.5);
-    const std::size_t jmax = static_cast<T>(0.75 * ysize + 0.5);
-    const std::size_t kmin = static_cast<T>(0.25 * zsize + 0.5);
-    const std::size_t kmax = static_cast<T>(0.75 * zsize + 0.5);
+    const std::size_t imin = 0.25 * xsize + 0.5;
+    const std::size_t imax = 0.75 * xsize + 0.5;
+    const std::size_t jmin = 0.25 * ysize + 0.5;
+    const std::size_t jmax = 0.75 * ysize + 0.5;
+    const std::size_t kmin = 0.25 * zsize + 0.5;
+    const std::size_t kmax = 0.75 * zsize + 0.5;
 
     for(std::size_t k = 0; k < zsize; ++k)
         for(std::size_t j = 0; j < ysize; ++j)
@@ -50,7 +50,7 @@ void write_file(std::ostream &os, const T *u_host,
     for(std::size_t k = 0; k < zsize; ++k)
         for(std::size_t j = 0; j < ysize; ++j)
             for(std::size_t i = 0; i < xsize; ++i)
-                os << std::setprecision(std::numeric_limits<double>::digits10)
+                os << std::setprecision(std::numeric_limits<T>::digits10)
                    << u_host[index(i, j, k, xsize, ysize)]
                    << ((k < zsize - 1 || j < ysize - 1 || i < xsize - 1) ? ',' : '\n');
 }
